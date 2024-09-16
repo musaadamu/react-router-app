f4bd1d4d8b02fd15fea62dc347e499be5e65e8db
Musa Adamu <msmajemusa4@gmail.com>
2024-09-16 14:21:32 +0100

    mirage js production

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['miragejs'], // Ensure miragejs is treated as external
+      external: process.env.NODE_ENV === 'production' ? ['miragejs'] : []
     }
   }
 });
